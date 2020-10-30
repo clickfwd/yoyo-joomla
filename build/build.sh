20cd7c1d50a45f85a3ea413c7755447f31abc179
Alejandro S <alejandro@jreviews.com>
2020-10-30 08:11:54 -0400

    Bump version to 0.3.1

--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,7 @@ rm build/templates/library/composer.lock
 mv build/templates/library/composer.json build/templates/library/composer-prod.json
 mv build/templates/library/composer-dev.json build/templates/library/composer.json
 
-composer install -d build/templates/library --no-dev
+composer install -d build/templates/library
 
 mv build/templates/library/composer.json build/templates/library/composer-dev.json
 mv build/templates/library/composer-prod.json build/templates/library/composer.json
